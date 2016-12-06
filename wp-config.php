48470114df6127a1adf362a27ac408445c4150fc
tier511 <work@tier5.us>
2016-12-06 20:02:51 +0530

    upload spread sheet forntend v1.0

--- wp-config.php
+++ wp-config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_NAME', 'coinzest_dev');
-define('DB_USER', 'coinzest_dev');
-define('DB_PASSWORD', 'Le8kEjau3.e');
+define('DB_NAME', 'coinzest');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
 define('DB_HOST', 'localhost');
 
 define('DB_CHARSET', 'utf8');
