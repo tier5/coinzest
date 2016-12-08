39a93ee9600fc9d8c7546e0ee97665dea6234a65
tier511 <work@tier5.us>
2016-12-08 19:42:49 +0530

    file upload baseline v1.0

--- platform/application/controllers/Uploadspreadsheet.php
+++ platform/application/controllers/Uploadspreadsheet.php
@@ -4,11 +4,13 @@ class Uploadspreadsheet extends CI_Controller {
 	
 	function __construct() {
 		parent::__construct();
-		$this->load->helper('url');
-		$this->load->library(array('user_agent', 'read_excel/Spreadsheet_Excel_Reader'));
+		$this->load->helper(array('url'));
+		$this->load->library(array('user_agent', 'read_excel/Spreadsheet_Excel_Reader', 'session'));
 	}
 
 	public function index() {
+		/*print_r($_COOKIE);
+		exit();*/
 		$this->load->model('daily_tasks_file_upload');
 		$file_name = array();
 		//getting the file name as $file_name[0]
@@ -20,11 +22,41 @@ class Uploadspreadsheet extends CI_Controller {
         	move_uploaded_file($tmp_name, "../file_uploads/$name");
         	error_reporting(E_ALL ^ E_NOTICE);
 			$data = new Spreadsheet_Excel_Reader("../file_uploads/".$name);
-			$this->daily_tasks_file_upload->index($data);
+			$query_res = $this->daily_tasks_file_upload->index($data, $file_name[0]);
+			/*print_r($query_res);
+			exit();*/
+			session_start();
+			switch ($query_res) {
+				case 1:
+					//success
+					$_SESSION["response"] = 1;
+					//$this->session->set_userdata('response',1);
+					break;
+				case -1:
+					//file already uploaded
+					$_SESSION["response"] = -1;
+					//$this->session->set_userdata('response',-1);
+					break;
+				case 0:
+					//fail to insert 2nd rec
+					$_SESSION["response"] = 0;
+					//$this->session->set_userdata('response',0);
+					break;
+				default:
+					// internal server error
+					$_SESSION["response"] = 500;
+					//$this->session->set_userdata('response',500);
+					break;
+			}
+			redirect($this->agent->referrer());
+			//$this->session->unset_userdata('response');
+			/*print_r($this->session->userdata());
+			exit();*/
 		} else {
 			//bad input here xlsx allowed
+			$_SESSION["response"] = 2;
+			//$this->session->set_userdata('response',2);
 			redirect($this->agent->referrer());
 		}
-		//print_r($file_name[0]);
 	}
 }
--- platform/application/models/Daily_tasks_file_upload.php
+++ platform/application/models/Daily_tasks_file_upload.php
@@ -1,6 +1,6 @@
 <?php
  /**
- * 
+ * @author Tier5 llc
  */
  class Daily_tasks_file_upload extends CI_Model
  {
@@ -8,19 +8,53 @@
     {
             // Call the CI_Model constructor
             parent::__construct();
+            $this->load->database();
     }
-    public function index($data) {
-    	print_r($data->val(2,1));
-    	exit();
-    	/*$data_to_push = array();
+    public function index($data, $update_date) {
+    	//search for that day in databse is any file uploaded
+    	$search_date = "SELECT * FROM file_on_date WHERE upload_date = '".$update_date."'";
+    	$check_date = $this->db->query($search_date);
+    	if (isset($check_date) && $check_date->row() != null) {
+    		if ($check_date->row()->upload_date === $update_date && $check_date->row()->is_active == 1) {
+	    		return -1;
+	    	} else {
+	    		//call that function
+	    		return self::databaseOperations($data, $update_date);
+	    	}
+    	} else {
+    		//call that function 
+    		return self::databaseOperations($data, $update_date);
+    	}
+    }
+    public function databaseOperations($data, $update_date) {
+    	//$ci =& get_instance();
+    	$some1 = "";
+    	$some2 = "";
+    	$some3 = "";
+    	$some4 = "";
+    	$some5 = "";
+    	$some6 = "";
     	for ($i=2; $i <= $data->rowcount(); $i++) { 
-				//for ($j=2; $j <= $data->colcount() ; $j++) { 
-					$sql = INSERT INTO `file_upload_log`(`update_date`, `timestamp`, `name`, `bmh_user_id`, `email`, `fb_account_url`, `date_of_1st_ph`, `first_ph_amount`, `task_one`, `task_one_details`, `task_one_id`, `modarator_comment_task_one`, `task_two`, `task_two_details`, `task_two_id`, `modarator_comment_task_two`, `current_ph`, `id`) VALUES ("somedata",$data->val($i,1),[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10],[value-11],[value-12],[value-13],[value-14],[value-15],[value-16],[value-17],[value-18])
-					print_r($data->val($i,$j));
-					echo "<br/>";
-				//}
-				echo "<br>";
-			}
-        	unlink('../file_uploads/'.$name);*/
-    }
+    		$some1 = explode(' ', $data->val($i,5))[0];
+	    	$some2 = explode(' ', $data->val($i,5))[1];
+	    	$some3 = explode(' ', $data->val($i,12))[1];
+	    	$some4 = explode(' ', $data->val($i,12))[0];
+	    	$some5 = $data->val($i,6) == 'DONE' ? 1 : 0;
+	    	$some6 = $data->val($i,10) == 'DONE' ? 1 : 0 ;
+			$sql = "INSERT INTO file_upload_log (update_date, name, bmh_user_id, fb_account_url, date_of_1st_ph, first_ph_amount, task_one,task_one_details, task_one_id, modarator_comment_task_one,task_two, task_two_details, task_two_id,modarator_comment_task_two, current_ph, date_of_ph_2nd) VALUES ('".$update_date."','".$data->val($i,2)."','".$data->val($i,3)."','".$data->val($i,4)."','".$some1."','".$some2."','".$some5."','".$data->val($i,7)."','".$data->val($i,8)."','".$data->val($i,9)."','".$some6."','".$data->val($i,11)."','".$data->val($i,13)."','".$data->val($i,14)."','".$some3."','".$some4."')";
+			$query = $this->db->query($sql);
+		}
+		$insert_date = "INSERT INTO file_on_date(upload_date,is_active) VALUES ('".$update_date."', 1)";
+		$run_query = $this->db->query($insert_date);
+		/*print_r($run_query);
+		exit();*/
+		if ($run_query == 1) {
+			return 1; //this is only valid
+		} else {
+			return 0;
+			//debug the sql error here dev guide -Tier5 llc
+			//$error = $this->db->error(); print $error;
+		}
+        unlink('../file_uploads/'.$update_date.'.xls');
+    } 
  }
